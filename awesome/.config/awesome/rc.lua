46c35256c8c2ef78e7f531a3eb9fc2f6b324a4bb
vincentvermeersch <sk8_vince@hotmail.fr>
2022-06-17 15:24:32 +0200

    update picom start

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -468,7 +468,7 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 beautiful.useless_gap = 5
 
 -- Autostart
-awful.spawn.with_shell("picom")
+awful.spawn.with_shell("picom --experimental-backends")
 awful.spawn.with_shell("/home/vincent/.config/polybar/launch.sh")
 awful.spawn.with_shell("feh --bg-fill -r -z ~/wallpapers")
 
