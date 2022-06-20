becf76cda8f7c51551e89f4464b42ea73535cf4b
vincentvermeersch <sk8_vince@hotmail.fr>
2022-06-20 14:18:28 +0200

    fix

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -108,7 +108,8 @@ menubar.utils.terminal = terminal -- Set the terminal for applications that requ
 
 awful.screen.connect_for_each_screen(function(s)
     -- Each screen has its own tag table.
-    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
+    -- awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
+    awful.tag({ "1", "2", "3", "4", "5"}, s, awful.layout.layouts[1])
 
 end)
 -- }}}
