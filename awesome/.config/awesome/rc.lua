3827f9fb49faa1205b69c5e855b27d140f6e8c48
Tatsuyasan <sk8_vince@hotmail.fr>
2024-01-04 10:55:12 +0100

    change pasystray init

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -3,9 +3,6 @@ require("awful.autofocus")
 -- load luarocks if installed
 pcall(require, "luarocks.loader")
 
--- local st = require("util.stalonetray")
--- st.start()
-
 require("themes")
 require("config")
 require("bindings")
@@ -17,6 +14,5 @@ awful.spawn.with_shell("xbanish")
 awful.spawn.with_shell("flameshot")
 awful.spawn.with_shell("nm-applet")
 awful.spawn.with_shell("blueman-applet")
-awful.spawn.with_shell("pasystray")
 awful.spawn.with_shell("~/.screenlayout/home.sh")
 awful.spawn.with_shell("feh --bg-fill -r -z ~/wallpapers")
