6368191779d4e8379ce7f1074186c74ece27fce7
Tatsuyasan <sk8_vince@hotmail.fr>
2024-01-09 12:39:38 +0100

    update theme

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -13,7 +13,9 @@ require("lazy").setup({
       "LazyVim/LazyVim",
       import = "lazyvim.plugins",
       opts = {
+        -- colorscheme = "solarized-osaka",
         colorscheme = "onedark",
+        -- colorscheme = "kanagawa",
         news = {
           lazyvim = true,
           neovim = true,
--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -1,23 +1,23 @@
 return {
-  {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    -- you can do it like this with a config function
-    config = function()
-      require("catppuccin").setup({
-        transparent_background = true,
-        integrations = {
-          cmp = true,
-          gitsigns = true,
-          nvimtree = true,
-          telescope = true,
-          notify = true,
-          mini = true,
-        },
-      })
-    end,
-  },
+  -- {
+  --   "catppuccin/nvim",
+  --   lazy = false,
+  --   name = "catppuccin",
+  --   -- you can do it like this with a config function
+  --   config = function()
+  --     require("catppuccin").setup({
+  --       transparent_background = true,
+  --       integrations = {
+  --         cmp = true,
+  --         gitsigns = true,
+  --         nvimtree = true,
+  --         telescope = true,
+  --         notify = true,
+  --         mini = true,
+  --       },
+  --     })
+  --   end,
+  -- },
   {
     "navarasu/onedark.nvim",
     lazy = false,
@@ -28,21 +28,21 @@ return {
       require("onedark").load()
     end,
   },
-  {
-    "rebelot/kanagawa.nvim",
-    lazy = false,
-    config = function()
-      require("kanagawa").setup()
-    end,
-  },
-  {
-    "craftzdog/solarized-osaka.nvim",
-    lazy = true,
-    priority = 1000,
-    opts = function()
-      return {
-        transparent = true,
-      }
-    end,
-  },
+  -- {
+  --   "rebelot/kanagawa.nvim",
+  --   lazy = false,
+  --   config = function()
+  --     require("kanagawa").setup()
+  --   end,
+  -- },
+  -- {
+  --   "craftzdog/solarized-osaka.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = function()
+  --     return {
+  --       transparent = false,
+  --     }
+  --   end,
+  -- },
 }
