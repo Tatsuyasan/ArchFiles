78c27f87ad2a10ec0873be77e3663e66d5ec8242
20104519 <20104519@hq.corp.leroymerlin.com@X504413.hq.corp.leroymerlin.com>
2024-01-12 20:27:31 +0100

    update screen tag creation

--- awesome/.config/awesome/signals/screen/init.lua
+++ awesome/.config/awesome/signals/screen/init.lua
@@ -1,36 +1,36 @@
-local awful = require 'awful'
-local beautiful = require 'beautiful'
-local wibox = require 'wibox'
-local naughty = require 'naughty'
+local awful = require("awful")
+local beautiful = require("beautiful")
+local wibox = require("wibox")
+local naughty = require("naughty")
 
-local vars = require 'config.vars'
+local vars = require("config.vars")
 
-screen.connect_signal('request::wallpaper', function(s)
-    awful.wallpaper {
-        screen = s,
-        widget = {
-            {
-                image     = beautiful.wallpaper,
-                upscale   = true,
-                downscale = true,
-                widget    = wibox.widget.imagebox,
-            },
-            valign = 'center',
-            halign = 'center',
-            tiled = false,
-            widget = wibox.container.tile,
-        }
-
-    }
+screen.connect_signal("request::wallpaper", function(s)
+	awful.wallpaper({
+		screen = s,
+		widget = {
+			{
+				image = beautiful.wallpaper,
+				upscale = true,
+				downscale = true,
+				widget = wibox.widget.imagebox,
+			},
+			valign = "center",
+			halign = "center",
+			tiled = false,
+			widget = wibox.container.tile,
+		},
+	})
 end)
 
-screen.connect_signal('request::desktop_decoration', function(s)
-    -- Each screen has its own layouts.
-    if s.index == 1 then
-        -- create tags for screen 1
-        awful.tag(vars.tags, s, vars.layouts.horizontal)
-    elseif s.index == 2 then
-        -- create tags for screen 2
-        awful.tag(vars.tags, s, vars.layouts.vertical)
-    end
+screen.connect_signal("request::desktop_decoration", function(s)
+	-- Each screen has its own layouts.
+	if s.index == 2 then
+		-- create tags for screen 2
+		awful.tag(vars.tags, s, vars.layouts.vertical)
+
+		return
+	end
+
+	awful.tag(vars.tags, s, vars.layouts.horizontal)
 end)
