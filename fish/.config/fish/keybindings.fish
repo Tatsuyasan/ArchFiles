ff1c7da1b5b68ed7b274c58eb3d0b58f33a935f6
20104519 <20104519@hq.corp.leroymerlin.com@X504413.hq.corp.leroymerlin.com>
2024-01-12 20:33:55 +0100

    update conf for ubuntu os

--- fish/.config/fish/keybindings.fish
+++ fish/.config/fish/keybindings.fish
@@ -1,6 +1,6 @@
 function fish_user_key_bindings
     bind \cg peco_select_ghq # Ctrl-g
     bind \cr peco_select_history # Ctrl-r
-    bind \cj peco_select_z # Ctrl-j
+    bind \cz peco_select_z # Ctrl-j
     bind \cf peco_select_file # Ctrl-f
 end
